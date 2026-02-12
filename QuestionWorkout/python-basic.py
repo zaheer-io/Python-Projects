2171006a8dcb9aa8bcf5bafdfbeb6068fb41ab64
zaheer-io <zaheerrs1234@gmail.com>
2026-02-12 11:45:05 +0530

    new

---
+++ QuestionWorkout/python-basic.py
@@ -0,0 +1,9 @@
+#p4
+
+num1 =  int(input("Enter a number: "))
+num2 = int(input('Enter second number: '))
+
+print(f'Sum is {num1 + num2}')
+print(f'Difference is {num1 - num2}')
+print(f'Product is {num1 * num2}')
+print(f'Quotient is {num1 / num2}')
